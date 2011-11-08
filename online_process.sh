241a2054ddb5296d96c6a3767e1ce10d5e32d4fd
Sho Uemura <meeg@slac.stanford.edu>
2011-11-08 18:47:55 +0100

    Fixed some bugs in scripts

--- online_process.sh
+++ online_process.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #octave 
-datadir="/media/ADATA"
-localdir="/home/meeg/matlab_online/temp"
+datadir="/media/ADATA/"
+tempdir="/home/meeg/matlab_online/temp/"
+localdir="/home/meeg/matlab_online/data/"
 while true
 do
-for i in `ls $datadir/*.wfm`
+for i in `ls $datadir*.wfm 2>/dev/null`
 do
 filename=`basename "$i"`
-echo "$filename"
-if [[ ! -f "$localdir/$filename" ]]
+if [[ (! -f "$localdir$filename") && (! -f "$tempdir$filename")  ]]
 then
 echo "copying $filename"
-cp "$datadir/$filename" "$localdir/$filename"
+cp "$datadir$filename" "$tempdir$filename"
 fi
 done
+echo "Done copying"
 sleep 1
 done
--- watch_data.m
+++ watch_data.m
@@ -5,7 +5,7 @@ for file=ls(temp)'
 %Make the filename into a column vector like Matlab expects
 file = file';
 [~, name, ext] = fileparts(file);
-printf('%s\n',ext)
+%printf('%s\n',ext)
 if strncmpi(ext,'.wfm',4)
 if strcmp(name(end-2:end),'Ch2')
 view_data(strcat(temp,file));
